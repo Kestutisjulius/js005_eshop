77d4ca1defc48eced1bd3be16b838dedc1cca31d
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-03-25 16:08:47 +0200

    print List

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('la - Hi');
+const jsonParse = require('./lib/jsonParse.js');
+const readFile = require('./lib/readFile.js');
+const printList = require('./lib/printList.js');
+
+
+
+(async () => {
+
+    const goods = ['arbata', 'kvepalai', 'masina', 'pomidoras','on','of', 5, [], ()=> {}, undefined, ''];
+
+    const goodsInfo = [];
+
+    // for (const item of goods) {
+    //     if (typeof item === 'string' && item !== '') {
+    //         const itemText = await readFile(item)
+    //         if (itemText !== false && itemText !== '') {
+    //            const itemObj = jsonParse(itemText);
+    //            if (itemObj !== false) {
+    //                goodsInfo.push(itemObj);
+
+    //            }
+    //         }        
+    //     }      
+    // }
+
+    for (const item of goods) {
+        if (typeof item !== 'string' || item == '') {
+                continue; }
+        const itemText = await readFile(item)
+        const itemObj = jsonParse(itemText);
+        if (itemObj == false) {
+            continue;}        
+        goodsInfo.push(itemObj);          
+    }
+
+
+    console.log("Univermagas pardavime turi..");
+    console.log("----------------------------");
+    console.log(printList(goodsInfo));
+    
+    
+    console.log("----------------------------");
+
+})();
+
